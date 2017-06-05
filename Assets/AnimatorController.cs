896752db7634e997af6ae2ae0bead25f27d9e657
Owen Morgan <o.morgan@bath.ac.uk>
2017-06-05 18:37:13 +0100

    Added mesh models, and enemies.

---
+++ Assets/AnimatorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
