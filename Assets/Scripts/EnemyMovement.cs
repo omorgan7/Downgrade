138bfb432abeab6a94a82dbebf8314c892943d95
azeemKhan9 <azeemkhan100@hotmail.com>
2017-06-06 10:09:46 +0100

    Commiting EnemyMovement script and SmartEnemy prefab

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform playerLoc;
+    GameObject player;
+    bool playerInRange;
+    Ray ray;
+    RaycastHit hit;
+    public float rotationSpeed = 3.0f, moveSpeed = 3.0f;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerLoc = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+            print("Trigger entered");
+        }
+    }
+
+
+    void Update()
+    {
+
+        if (playerInRange)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerLoc.position - transform.position), rotationSpeed * Time.deltaTime);
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            playerInRange = false;
+        }
+    }
+}
